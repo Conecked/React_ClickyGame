9c9013a558a819aaccc9e5c6d75a51a707634dae
Armando <xxnowonxx@gmail.com>
2018-10-01 15:15:20 -0700

    updated css

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,7 +5,7 @@ const Nav = props => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
     <ul>
       <li>
-      <a className="navbar-brand" href="/React_ClickyGame">Vampirina Clicky Game</a>
+      <a className="navbar-brand" href="/">Vampirina Clicky Game</a>
       </li>
       <li>
         <h1>Score  {props.score}  |  Top Score  {props.topScore}</h1>
